da2aa94a720a29ac1b650c7146cd380b5705c8e8
Ivan Kobtsev <vanomc77@gmail.com>
2023-04-03 16:41:20 +0600

    added gitignore

---
+++ GabionCalculator.DAL/Entities/Enums/MaterialKind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabionCalculator.DAL.Entities.Enums
+{
+    public enum MaterialKind
+    {
+        VR1 = 0, // проволока ВР1
+        OK = 1, // проволока ОК
+        Т_O = 2, // проволока термически обработанная
+        ZINK = 3, // проволока оцинкованная
+        KNIT = 4 // проволока вязальная
+    }
+}
