202b3ffd43238a52cbde64c0591207de2a47002a
Ivan Kobtsev <vanomc77@gmail.com>
2023-04-04 08:13:18 +0600

    added applicationContext

--- GabionCalculator.DAL/Data/ApplicationContext.cs
+++ GabionCalculator.DAL/Data/ApplicationContext.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using GabionCalculator.DAL.Entities.Common.Interfaces;
+using GabionCalculator.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace GabionCalculator.DAL.Data
 {
-    internal class ApplicationContext
+    public class ApplicationContext : DbContext
     {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Gabion> Gabions { get; set; }
+        public DbSet<Material> Materials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SetNullBehaviour(builder);
+        }
+
+        private static void SetNullBehaviour(ModelBuilder builder)
+        {
+            builder.Entity<Gabion>()
+             .HasOne(b => b.Material)
+             .WithMany(a => a.Gabions)
+             .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<Gabion>()
+             .HasOne(b => b.User)
+             .WithMany(a => a.Gabions)
+             .OnDelete(DeleteBehavior.SetNull);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            foreach (var entry in ChangeTracker.Entries<IDateFixEntity>())
+            {
+                entry.Entity.DateStart = entry.State switch
+                {
+                    EntityState.Added => DateTime.Now,
+                    _ => entry.Entity.DateStart
+                };
+                entry.Entity.DateUpdate = entry.State switch
+                {
+                    EntityState.Modified => DateTime.Now,
+                    _ => entry.Entity.DateStart
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
