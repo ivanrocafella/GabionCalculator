443ce20548fe1e8255caf00901d8d2ded2a2a639
Ivan Kobtsev <vanomc77@gmail.com>
2023-07-10 09:13:20 +0600

    turn of https redirect

--- GabionCalculator.API/Program.cs
+++ GabionCalculator.API/Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
     
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
