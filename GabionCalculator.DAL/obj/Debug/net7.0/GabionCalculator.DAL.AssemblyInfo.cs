2e371ad9b65b64258421b6376217846f54787bbc
Ivan Kobtsev <vanomc77@gmail.com>
2025-01-06 15:18:09 +0600

    added allowed hosts

--- GabionCalculator.DAL/obj/Debug/net7.0/GabionCalculator.DAL.AssemblyInfo.cs
+++ GabionCalculator.DAL/obj/Debug/net7.0/GabionCalculator.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GabionCalculator.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8d3aa3a773a1b5be46d623b2b94e64417ecafb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GabionCalculator.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GabionCalculator.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
